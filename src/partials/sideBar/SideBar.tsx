8f12eb7e3e82958cef7665ec9e080406d2b2dbe1
LA4 <andr.ludow@gmail.com>
2024-01-04 13:21:37 +0100

    Sidebar - open and close functionality

--- src/partials/sideBar/SideBar.tsx
+++ src/partials/sideBar/SideBar.tsx
@@ -1,22 +1,53 @@
+import { AnimatePresence, useAnimate } from "framer-motion";
 import { LeftDoubleArrow, RightDoubleArrow } from "../../assets/DoubleArrow";
 import "./sidebar.css";
 import { useState } from "react";
 export function SideBar() {
-  const [open, setOpen] = useState<Boolean>(false);
+  const [open, setOpen] = useState<Boolean>(true);
+  const [scope, animate] = useAnimate();
   const handleOpen = () => {
-    setOpen(!open);
-  };
-  const openStyle = {
-    width: open ? "250px" : " 15px",
+    if (open) {
+      animate([
+        [".side-container-child", { x: -100, width: "0" }, { duration: 0.3 }],
+        [
+          scope.current,
+          { opacity: 1, width: "0px" },
+          { duration: 0.3, at: "<" },
+        ],
+      ]);
+      setOpen(!open);
+    } else {
+      animate([
+        [
+          scope.current,
+          { x: 0, opacity: 1, width: "250px" },
+          { duration: 0.3 },
+        ],
+        [
+          ".side-container-child",
+          { x: 0, width: "100%", display: "none" },
+          { duration: 0.3, at: "<" },
+        ],
+      ]);
+      setOpen(!open);
+    }
   };
   return (
     <>
-      <div style={openStyle} className="side-container">
-        {open && <div className="side-container-child">sidebar</div>}
+      <AnimatePresence>
+        <div ref={scope} className="side-container">
+          <div className="side-container-child">sidebar</div>
+        </div>
+      </AnimatePresence>
+      {open ? (
         <span onClick={handleOpen} className="oppener">
-          {open ? <LeftDoubleArrow /> : <RightDoubleArrow />}
+          <LeftDoubleArrow />
         </span>
-      </div>
+      ) : (
+        <span onClick={handleOpen} className="oppener">
+          <RightDoubleArrow />
+        </span>
+      )}
     </>
   );
 }
