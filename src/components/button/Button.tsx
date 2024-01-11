1f82b673c027a251d0058f42b900a4890b6fe02a
LA4 <andr.ludow@gmail.com>
2024-01-11 11:28:12 +0100

    naute conaite & collection placement

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -4,6 +4,8 @@ type Props = {
   color?: string | undefined;
   width?: string | number | undefined;
   height?: string | number | undefined;
+  fontSize?: string | undefined;
+  fontWeight?: string | undefined;
   active?: boolean | undefined;
   props?: Props;
 };
@@ -14,12 +16,16 @@ export function Button({
   width,
   height,
   active,
+  fontSize,
+  fontWeight,
   ...props
 }: Props) {
   const buttonStyle = {
     color: color,
     width: width,
     height: height,
+    fontSize: fontSize,
+    fontWeight: fontWeight,
   };
   return (
     <>
--- src/partials/nauteContainer/nautecontainer.tsx
+++ src/partials/nauteContainer/nautecontainer.tsx
@@ -1,10 +1,24 @@
+import { Button } from "../../components/button/Button";
 import { Collection } from "../collection/Collection";
 import "./nautecontainer.css";
 export function NauteContainer() {
   return (
     <>
       <div className="naute-container">
-        <Collection></Collection>
+        <Button
+          width="250px"
+          height="29px"
+          fontSize=".9rem"
+          fontWeight="600"
+          active={true}
+        >
+          ajouter une collection
+        </Button>
+        <div className="naute-collection-container">
+          <Collection></Collection>
+          <Collection></Collection>
+          <Collection></Collection>
+        </div>
       </div>
     </>
   );
